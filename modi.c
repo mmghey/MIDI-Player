59b5d364ce740263009a076708cdc3f1fecb5aed
mmghey <57009278+mmghey@users.noreply.github.com>
2020-01-04 16:09:11 +0330

    Update modi.c

--- modi.c
+++ modi.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<math.h>
 FILE * midiFile;
 char chunklen[10];
@@ -41,15 +42,22 @@ void getChunks (void) {
 	printf("number of tracks = %d\n", ntracksI);
 	readBytes(2, tickdiv);
 	printf("tickdiv = %d\n", tickdiv[0] * 256 + tickdiv[1]);
+	if((format[1] == 0 && ntracksI > 1) || (format[1] == 1 && ntracksI < 2))  {
+		printf("Invalid MIDI file because the format and number of tracks doesn\'t match");
+	}
+	if(strcmp("MThd", identifier) != 0) {
+		printf("Invalid MIDI file because the file does not have header chunk");
+	}
 	for(c = 0 ; c < ntracksI ; c++) {
 		readBytes(4,chunks[c].identifier);
 		printf("\n");
 		puts(chunks[c].identifier);
 		readBytes(4,chunks[c].chunklen.s);
 		chunks[c].chunklen.in = chunks[c].chunklen.s[3] + chunks[c].chunklen.s[2]*pow(16,2) + chunks[c].chunklen.s[1]*pow(16,4) + chunks[c].chunklen.s[0]*pow(16,6);
-		printf("%dth chunklen = %d\n", c + 1, chunks[c].chunklen.in);
+		printf("chunklen %d = %d\n", c + 1, chunks[c].chunklen.in);
 		readBytes(chunks[c].chunklen.in,chunks[c].chunkData);
 	}
+
 
 }
 int readBytes (int len , char name[]) {
@@ -57,5 +65,8 @@ int readBytes (int len , char name[]) {
 	for(i = 0 ; i < len ; i++) {
 		fscanf(midiFile, "%c", &name[i]);
 	}
+}
+int getEvents (int len, char name[]) {
+
 }
 
